9ee37371e22e6c5431d62cf7120cf0cb07da6235
Igor Nikiforov <kolobamanacas@outlook.com>
2021-07-16 00:10:37 +0300

    Test linting

--- src/components/News/NewsList.vue
+++ src/components/News/NewsList.vue
@@ -1,38 +1,36 @@
 <template>
-    <el-row :gutter="40">
-        <el-col :xl="6" :lg="1">
-           <NewsCalendar v-model:news="news" />
+  <el-row :gutter="40">
+    <el-col :xl="6" :lg="1">
+      <NewsCalendar v-model:news="news" />
+    </el-col>
+    <el-col :span="18">
+      <el-row :gutter="20">
+        <el-col :xl="8" :lg="12" v-for="item in news" :key="item.id">
+          <NewsCard :news="item" />
         </el-col>
-        <el-col :span="18" >
-            <el-row :gutter="20">
-                <el-col :xl="8" :lg="12" v-for="item in news" :key="item.id" >
-            <NewsCard :news="item"/>
-                </el-col>
-            </el-row>
-        </el-col>
-    </el-row>
+      </el-row>
+    </el-col>
+  </el-row>
 </template>
 
 <script lang="ts">
-    import { useStore  } from 'vuex';
-    import NewsCard from "@/components/News/NewsCard.vue";
-    import NewsCalendar from "@/components/News/NewsCalendar.vue";
-    import { computed} from "vue";
+import { useStore } from 'vuex';
+import NewsCard from '@/components/News/NewsCard.vue';
+import NewsCalendar from '@/components/News/NewsCalendar.vue';
+import { computed } from 'vue';
 
-    export default {
-        name: 'NewsList',
-        components: {NewsCalendar, NewsCard},
-        async setup() {
-            const store = useStore();
-            await store.dispatch('news/getAll');
-            const news = computed(()=>store.getters['news/news'])
-            return {
-                news,
-            };
-        },
+export default {
+  name: 'NewsList',
+  components: { NewsCalendar, NewsCard },
+  async setup() {
+    const store = useStore();
+    await store.dispatch('news/getAll');
+    const news = computed(() => store.getters['news/news']);
+    return {
+      news,
     };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
