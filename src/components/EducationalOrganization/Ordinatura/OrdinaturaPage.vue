003a4af911120df0e642af14b3cc5aa8f8854454
Syrtsev <sirtsevilya@yandex.ru>
2022-01-18 19:47:43 +0300

    Update ordinatura menu. Version 2

--- src/components/EducationalOrganization/Ordinatura/OrdinaturaPage.vue
+++ src/components/EducationalOrganization/Ordinatura/OrdinaturaPage.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    <div class="card-item">
-      <h1>Ординатура</h1>
-      <div class="ordinatura-menu">
-        <div v-for="item in menu" :key="item.name" class="ordinatura-menu-item" @click="changeTab(item.name)">
-          <div :class="isActive(item.name)">{{ item.name }}</div>
+  <div class="ordinatura-page-container">
+    <div class="side-container">
+      <div class="side-item">
+        <div class="card-item">
+          <h4>Ординатура</h4>
+          <el-divider />
+          <el-table :data="menu" cell-class-name="cell-row" :show-header="false">
+            <el-table-column>
+              <template #default="scope">
+                <div :class="isActive(scope.row.name)" @click="changeTab(scope.row.name)">
+                  {{ scope.row.name }}
+                </div>
+              </template>
+            </el-table-column>
+          </el-table>
         </div>
       </div>
-
+    </div>
+    <div class="content-container">
       <!-- <el-tabs type="border-card" :active-name="pageTitle" :before-leave="test">
       <el-tab-pane label="Текущая информация для поступающих" name="Этапы поступления"><OrdinaturaStages /></el-tab-pane>
       <el-tab-pane label="Документы для поступления" name="Документы для поступления"><OrdinaturaDocumentsForAdmission /></el-tab-pane>
@@ -15,10 +25,10 @@
       <el-tab-pane label="План набора и поданные заявления" name="План набора и поданные заявления"><OrdinaturaExams /></el-tab-pane>
       <el-tab-pane label="Образцы документов" name="Образцы документов"><OrdinaturaExams /></el-tab-pane>
     </el-tabs> -->
+      <OrdinaturaStages v-if="activeMenuName === 'Текущая информация для поступающих'" />
+      <OrdinaturaDocumentsForAdmission v-if="activeMenuName === 'Документы для поступления'" />
+      <OrdinaturaExams v-if="activeMenuName === 'Вступительные испытания'" />
     </div>
-    <OrdinaturaStages v-if="activeMenuName === 'Текущая информация для поступающих'" />
-    <OrdinaturaDocumentsForAdmission v-if="activeMenuName === 'Документы для поступления'" />
-    <OrdinaturaExams v-if="activeMenuName === 'Вступительные испытания'" />
   </div>
 </template>
 
@@ -67,29 +77,67 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 @import '@/assets/styles/elements/ordinatura.scss';
-.ordinatura-menu {
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
+$side-cotainer-max-width: 300px;
+$content-max-width: 1000px;
+$card-margin-size: 30px;
+
+h4 {
+  margin: 0;
 }
-.anticon {
-  font-size: 40px;
-  margin-right: 10px;
+.el-divider {
+  margin: 10px 0 0;
 }
-.ordinatura-menu-item {
+:deep(.cell) {
+  padding: 0 !important;
+}
+:deep(.cell-row) {
+  cursor: pointer;
+}
+.ordinatura-page-container {
   display: flex;
-  align-items: center;
-  padding: 10px;
-  margin: 5px;
-  border: 1px solid rgb(black, 0.1);
-  border-radius: 5px;
-  background-color: #f5f6f8;
-  &:hover {
-    cursor: pointer;
-    background-color: darken(#f5f6f8, 1.2);
-    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+  justify-content: center;
+  width: 100%;
+}
+.side-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: $side-cotainer-max-width;
+  margin-right: $card-margin-size;
+
+  .side-item {
+    margin-bottom: $card-margin-size;
   }
 }
+
+.content-container {
+  max-width: $content-max-width;
+  width: 100%;
+}
+
+// .ordinatura-menu {
+//   display: flex;
+//   flex-wrap: wrap;
+//   align-items: flex-start;
+// }
+// .anticon {
+//   font-size: 40px;
+//   margin-right: 10px;
+// }
+// .ordinatura-menu-item {
+//   display: flex;
+//   align-items: center;
+//   padding: 10px;
+//   margin: 5px;
+//   border: 1px solid rgb(black, 0.1);
+//   border-radius: 5px;
+//   background-color: #f5f6f8;
+//   &:hover {
+//     cursor: pointer;
+//     background-color: darken(#f5f6f8, 1.2);
+//     box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+//   }
+// }
 .is-active {
   color: #42a4f5;
 }
