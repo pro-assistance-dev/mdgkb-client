b2895329241ec72405f89e28797a4085d96f0673
LakkinziMusic <lakkinzimusic@gmail.com>
2022-02-16 11:18:02 +0300

    Medical profile page draft

--- src/components/MedicalProfiles/MedicalProfilesPage.vue
+++ src/components/MedicalProfiles/MedicalProfilesPage.vue
@@ -1,23 +1,79 @@
 <template>
-  <div v-if="mounted">
-    <h1>{{ medicalProfile.name }}</h1>
-    <div class="wrapper">
-      <NewsSlider :news="medicalProfile.medicalProfilesNews" />
+  <div v-if="mounted" class="medical-profile-page-container">
+    <div class="side-container hidden-md-and-down">
+      <div v-if="medicalProfile.medicalProfilesDivisions.length" class="side-item">
+        <div class="card-item">
+          <h4>Отделения по профилю</h4>
+          <el-divider />
+
+          <el-table :data="medicalProfile.medicalProfilesDivisions" cell-class-name="cell-row" :show-header="false">
+            <el-table-column>
+              <template #default="scope">
+                <div class="recent-news-item" @click="$router.push(`/divisions/${scope.row.division.id}`)">
+                  <div class="item-title">{{ scope.row.division.name }}</div>
+                </div>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="recent-news-footer">
+            <button @click="$router.push('/divisions')">Все отделения</button>
+          </div>
+        </div>
+      </div>
+      <div v-if="medicalProfile.medicalProfilesNews.length" class="side-item">
+        <div class="card-item">
+          <h4>Новости медицинского профиля</h4>
+          <el-divider />
+          <el-table :data="medicalProfile.medicalProfilesNews" cell-class-name="cell-row" :show-header="false">
+            <el-table-column>
+              <template #default="scope">
+                <div class="recent-news-item" @click="$router.push(`/news/${scope.row.news.slug}`)">
+                  <div class="item-title">{{ scope.row.news.title }}</div>
+                  <div class="item-footer">
+                    <div class="item-date">{{ $dateFormatRu(scope.row.news.publishedOn, true) }}</div>
+                    <div class="icon">
+                      <EyeOutlined />
+                      <span>{{ scope.row.news.viewsCount }}</span>
+                    </div>
+                  </div>
+                </div>
+              </template>
+            </el-table-column>
+          </el-table>
+          <div class="recent-news-footer">
+            <button @click="$router.push('/news')">Все новости</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="medical-profile-content-container">
+      <div class="card-item" style="padding: 30px">
+        <div class="card-header">
+          <h2 class="title article-title">{{ medicalProfile.name }}</h2>
+          <div class="title-icon">
+            <BaseIcon width="150" height="150" :color="chooseRandomBrandColor()" icon-name="2"><icon-2 /></BaseIcon>
+          </div>
+        </div>
+        <el-divider />
+        <div class="article-body" v-html="medicalProfile.description"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { EyeOutlined } from '@ant-design/icons-vue';
 import { computed, ComputedRef, defineComponent, onBeforeMount, Ref, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { useStore } from 'vuex';
 
-import NewsSlider from '@/components/NewsSlider.vue';
+import BaseIcon from '@/components/Base/MedicalIcons/BaseIconMedicalProfiles.vue';
+import Icon2 from '@/components/Base/MedicalIcons/icons/2.vue';
 import IMedicalProfile from '@/interfaces/IMedicalProfile';
-
+import chooseRandomBrandColor from '@/mixins/brandColors';
 export default defineComponent({
   name: 'MedicalProfileList',
-  components: { NewsSlider },
+  components: { BaseIcon, Icon2, EyeOutlined },
   setup() {
     const store = useStore();
     const route = useRoute();
@@ -29,78 +85,139 @@ export default defineComponent({
       mounted.value = true;
     });
 
-    return {
-      mounted,
-      medicalProfile,
-    };
+    return { chooseRandomBrandColor, mounted, medicalProfile };
   },
 });
 </script>
 
-<style lang="scss" scoped>
-h1 {
+<style scoped lang="scss">
+$side-container-max-width: 300px;
+$medical-profile-content-max-width: 1000px;
+$card-margin-size: 30px;
+
+.title-icon {
   text-align: center;
 }
 
-.donor-card {
-  width: 400px;
-  border-radius: 15px;
-  margin: 10px;
-  color: #4a4a4a;
-  font-size: 14px;
-  position: relative;
+.medical-profile-page-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: $card-margin-size 0;
+}
 
-  &:hover {
-    cursor: pointer;
+.side-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: $side-container-max-width;
+  margin-right: $card-margin-size;
+
+  .side-item {
+    margin-bottom: $card-margin-size;
+  }
+}
+
+.medical-profile-content-container {
+  max-width: $medical-profile-content-max-width;
+  width: 100%;
+}
+
+.add-comment {
+  margin: 50px 0 50px 0;
+}
+
+.box-card {
+  margin: 10px 0 10px 0;
+}
+
+h2,
+h3 {
+  margin-top: 0;
+  color: black;
+  text-align: center;
+}
+h3 {
+  font-size: 20px;
+}
+
+.card-content {
+  margin-left: auto;
+  margin-right: auto;
+}
+
+:deep(p) {
+  text-align: justify;
+}
+
+:deep(a) {
+  color: inherit !important;
+}
+
+:deep(blockquote) {
+  border-left: 5px solid #2467a6;
+  margin-left: 0;
+  padding-left: 50px;
+}
+
+.card-item {
+  padding: 0;
+}
+.recent-news-item {
+  display: flex;
+  flex-direction: column;
+  .item-title {
+    font-weight: 600;
+  }
+  .item-footer {
+    display: flex;
+    justify-content: space-between;
     margin-top: 5px;
   }
-
-  .donor-img-container {
-    margin: 0 10px 10px 0;
-    img {
-      width: 100%;
-    }
-    .star-icon-container {
-      position: absolute;
-      top: 5px;
-      right: 30px;
-      transition: transform 0.2s;
-      &:hover {
-        cursor: pointer;
-        transform: scale(1.1);
-      }
-      .icon {
-        font-size: 30px;
-        margin: 5px;
-        background-color: white;
-      }
-      .favor {
-        color: orange;
-      }
-    }
+  .icon {
+    user-select: none;
+    display: flex;
+    align-items: center;
+    transition: all 0.2s;
+    margin-right: 3px;
   }
-  .donor-name {
+  .anticon {
+    padding-right: 5px;
     font-size: 16px;
-    font-weight: 600;
-    cursor: pointer;
   }
 }
+h4 {
+  color: black;
+  margin: 15px 0 0 10px;
+}
+.item-footer {
+  color: #a1a7bd;
+}
 
-h2 {
+:deep(.cell-row) {
+  cursor: pointer;
+}
+.el-divider {
+  margin: 10px 0 0;
+}
+// :deep(.cell) {
+// padding: 0 !important;
+// }
+.recent-news-footer {
+  margin: 10px;
+  margin-bottom: 15px;
   text-align: center;
-}
-.wrapper {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-}
-
-.scale-image-container {
-  width: 100%;
-  height: 100%;
-}
-.scale-image {
-  width: 100%;
-  height: 100%;
+  button {
+    background-color: white;
+    border-radius: 10px;
+    padding: 7px 20px;
+    font-size: 14px;
+    border: 1px solid #4a4a4a;
+    transition: background-color 0.25s ease;
+    &:hover {
+      cursor: pointer;
+      background-color: darken(white, 10%);
+    }
+  }
 }
 </style>
