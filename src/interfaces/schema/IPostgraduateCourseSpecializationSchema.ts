3af57b01cccf76f5ba111deeb42605a9aeeac091
LakkinziMusic <lakkinzimusic@gmail.com>
2022-04-13 19:45:48 +0300

    Lost files

---
+++ src/interfaces/schema/IPostgraduateCourseSpecializationSchema.ts
@@ -0,0 +1,7 @@
+export default interface IPostgraduateCourseSpecializationSchema {
+  tableName: string;
+  key: string;
+  id: string;
+  specializationId: string;
+  postgraduateCourseId: string;
+}
