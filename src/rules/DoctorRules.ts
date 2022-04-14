32d4343cdb110c9e3e9a3f8df957d738ee178e58
LakkinziMusic <lakkinzimusic@gmail.com>
2022-04-14 11:22:33 +0300

    Doctor prop fix

--- src/rules/DoctorRules.ts
+++ src/rules/DoctorRules.ts
@@ -2,7 +2,7 @@ import HumanRules from '@/rules/HumanRules';
 
 export default {
   education: [{ required: true, message: 'Необходимо указать образование', trigger: 'blur' }],
-  position: [{ required: true, message: 'Необходимо указать должность', trigger: 'blur' }],
+  // position: [{ required: true, message: 'Необходимо указать должность', trigger: 'blur' }],
   schedule: [{ required: true, message: 'Необходимо указать график работы', trigger: 'blur' }],
   // tags: [{ required: true, message: 'Необходимо указать тэги', trigger: 'blur' }],
   human: HumanRules,
