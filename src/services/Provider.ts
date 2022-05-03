0fa7cf3c74c91dbddd548644808f9baa371d4cc1
LakkinziMusic <lakkinzimusic@gmail.com>
2022-05-03 15:54:07 +0300

    Human sanitize

--- src/services/Provider.ts
+++ src/services/Provider.ts
@@ -1,10 +1,26 @@
+import { computed, ComputedRef, Ref } from 'vue';
+
 type CallbackFunction = () => void;
+import IFilterModel from '@/interfaces/filters/IFilterModel';
+import IFilterQuery from '@/interfaces/filters/IFilterQuery';
+import ISchema from '@/interfaces/schema/ISchema';
+
 import router from '../router/index';
 import store from '../store/index';
 const Provider = (() => {
   const r = router;
   const s = store;
+  const schema: Ref<ISchema> = computed(() => s.getters['meta/schema']);
+  const filterQuery: ComputedRef<IFilterQuery> = computed(() => store.getters['filter/filterQuery']);
+
+  function setFilterModel(model: IFilterModel): void {
+    s.commit('filter/setFilterModel', model);
+  }
+
   return {
+    setFilterModel: setFilterModel,
+    filterQuery: filterQuery,
+    schema: schema,
     router: r,
     store: s,
   };
