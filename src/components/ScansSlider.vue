3094a427b61d49df93fea9c2770e3da259988216
Daniel <qwer.82@mail.ru>
2022-09-01 10:55:56 +0300

    upd style for doctor certificate

--- src/components/ScansSlider.vue
+++ src/components/ScansSlider.vue
@@ -1,14 +1,15 @@
 <template>
-  <div v-if="carousel.length" class="education">
+  <div v-if="carousel.length" class="certificate">
     <div class="title-in">Сертификаты</div>
     <div v-if="carousel.length > 0" class="gallery-container">
       <el-carousel ref="carouselRef" v-touch:swipe="(direction) => $carouselSwipe(direction, carouselRef)" arrow="always" :interval="4000">
         <el-carousel-item v-for="certificatesGroups in carousel" :key="certificatesGroups">
           <div v-for="certificate in certificatesGroups" :key="certificate.id" class="image-container">
-            <img
+            <img 
+              class="image-card"
               :src="certificate.scan.getImageUrl()"
               :alt="certificate.scan.originalName"
-              :style="{ height: '80%', width: '80%' }"
+              :style="{ height: '100%', width: '100%' }"
               @click="imgClickHandler(certificate.scan)"
             />
           </div>
@@ -71,17 +72,8 @@ export default defineComponent({
 });
 </script>
 <style scoped lang="scss">
-.image-container {
-  width: 220px;
-  height: 320px;
-}
+@import '@/assets/styles/elements/base-style.scss';
 
-:deep(.el-carousel__item) {
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-}
 * {
   padding: 0px;
   margin: 0px;
@@ -102,7 +94,7 @@ html {
   scroll-behavior: smooth;
 }
 
-.education {
+.certificate {
   display: block;
   background: #ffffff;
   border-radius: 5px;
@@ -110,6 +102,7 @@ html {
   background-clip: padding-box;
   margin-top: 30px;
   padding: 0px 40px 0px 23px;
+  // height: 460px;
 }
 
 .title-in {
@@ -123,74 +116,35 @@ html {
   font-weight: bold;
 }
 
-.point {
+.image-card {
+    transition: all 0.3s;
+}
+
+.image-card:hover {
+  box-shadow: $normal-shadow;
+  cursor: pointer;
+}
+
+
+
+.image-container {
+  width: 220px;
+  height: 320px;
+}
+
+:deep(.el-carousel__container) {
+  height: 370px;
+  // height: unset;
+}
+
+:deep(.el-carousel__item) {
   display: flex;
-  margin: 0px 0px 20px 20px;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
 }
 
-.point-info {
-  display: block;
-  height: auto;
-  margin: 10px 0px 0px 5px;
-}
-
-.point-year {
-  display: flex;
-  font-family: Comfortaa, Arial, Helvetica, sans-serif;
-  letter-spacing: 0.1em;
-  font-size: 14px;
-  color: #a1a7bd;
-  font-weight: bold;
-}
-
-.point-text {
-  display: flex;
-  font-family: Comfortaa, Arial, Helvetica, sans-serif;
-  letter-spacing: 0.1em;
-  font-size: 11px;
-  color: #5d6477;
-  font-weight: lighter;
-  line-height: 110%;
-}
-
-/* Decoration of lists */
-
-.point-list {
-  padding-left: 0;
-  margin: 0;
-  list-style: none;
-}
-
-.point-list-item {
-  margin-bottom: 1rem;
-  padding-left: 25px;
-  position: relative;
-}
-
-.point-list-item:before {
-  content: '';
-  position: absolute;
-  top: 3px;
-  left: 0;
-  width: 10px;
-  height: 10px;
-  border: 2px solid #a1a7bd;
-  background-color: #ffffff;
-  border-radius: 50%;
-  z-index: 2;
-}
-
-.point-list-item:after {
-  content: '';
-  position: absolute;
-  top: -8px;
-  left: 6px;
-  width: 2px;
-  height: calc(100% + 3rem);
-  background-color: #a1a7bd;
-}
-
-.point-list-item:last-of-type:after {
-  height: calc(100% + 1.3rem);
+:deep(.el-carousel__button) {
+  background-color: $site_gray;
 }
 </style>
