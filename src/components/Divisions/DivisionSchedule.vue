71a476711d2f928e627f977923d4a8855cc41d31
LakkinziMusic <lakkinzimusic@gmail.com>
2022-09-14 07:29:07 +0300

    Hide empty division shedule block

--- src/components/Divisions/DivisionSchedule.vue
+++ src/components/Divisions/DivisionSchedule.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- <div v-if="educations.length" class="education"> -->
   <!-- <div v-if="division.schedule.scheduleItems.length > 0" class="education"> -->
-  <div class="schedule">
+  <div v-if="division.schedule.scheduleItems.length || division.visitingRules.length" class="schedule">
     <div class="title-in">Распорядок дня</div>
     <div class="block">
       <div class="left-block">
