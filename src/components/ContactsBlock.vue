87c29b1deda486d18c9551f035cb8e4f7a32b153
Daniel <qwer.82@mail.ru>
2022-10-16 23:55:25 +0300

    add worktime

--- src/components/ContactsBlock.vue
+++ src/components/ContactsBlock.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="contactInfo.telephoneNumbers.length > 0" class="contact-h3">
+  <div v-if="contactInfo.telephoneNumbers[0].number" class="contact-h3">
     <div class="item">
       <svg class="icon-phone">
         <use xlink:href="#phone"></use>
@@ -9,7 +9,7 @@
       <div v-for="phone in contactInfo.telephoneNumbers" :key="phone.id" class="item">{{ phone.number }}</div>
     </div>
   </div>
-  <div class="contact-h3">
+  <div v-if="contactInfo.emails[0].address" class="contact-h3">
     <div class="item">
       <svg class="icon-email">
         <use xlink:href="#email"></use>
@@ -54,5 +54,48 @@ export default defineComponent({
 
 .division-name {
   margin: 0px;
+}
+
+.icon-phone {
+  width: 20px;
+  height: 20px;
+  fill: #2754eb;
+}
+
+.icon-email {
+  width: 20px;
+  height: 20px;
+  fill: #2754eb;
+}
+
+.icon-map-marker {
+  margin-left: -1px;
+  width: 23px;
+  height: 23px;
+  fill: #2754eb;
+}
+
+.contact-h3 {
+  display: flex;
+  justify-content: left;
+  font-family: Roboto, Verdana, sans-serif;
+  font-size: 12px;
+  font-weight: lighter;
+  color: #4a4a4a;
+  align-content: center;
+  text-align: center;
+  margin-top: 5px;
+}
+
+.item {
+  display: flex;
+  flex-direction: row;
+  padding-right: 10px;
+  width: auto;
+  align-items: center;
+  text-align: left;
+}
+.hidden {
+  display: none;
 }
 </style>
--- src/components/Educational/Education/StructurePage.vue
+++ src/components/Educational/Education/StructurePage.vue
@@ -39,9 +39,20 @@
                 <el-avatar :size="200" :src="manager.doctor.human.photoMini.getImageUrl()"></el-avatar>
               </div>
               <div class="doctor-info">
-                <h4>{{ manager.doctor.human.getFullName() }}</h4>
+                <h4 class="doctor-name">{{ manager.doctor.human.getFullName() }}</h4>
                 <p>{{ manager.role }}</p>
-                
+                <ContactsBlock :contact-info="manager.doctor.human.contactInfo" />
+                <div class="contact-h3">
+                  <div class="item">
+                    <svg  class="icon-time">
+                      <use xlink:href="#time"></use>
+                    </svg>
+                  </div>
+                  <div class="time-block">
+                    <span class="item">Вт: с 11:00 до 13:00</span>
+                    <span class="item">Пн: с 11:00 до 13:00</span>
+                  </div>
+                </div>
               </div>
             </div>
           </el-card>
@@ -49,6 +60,7 @@
       <!-- </el-timeline> -->
     </el-card>
   </el-container>
+  <Time />
 </template>
 
 <script lang="ts">
@@ -56,9 +68,12 @@ import { computed, defineComponent, onBeforeMount, Ref, ref } from 'vue';
 import { useStore } from 'vuex';
 
 import IEducationalOrganization from '@/interfaces/IEducationalOrganization';
+import ContactsBlock from '@/components/ContactsBlock.vue';
+import Time from '@/assets/svg/StructurePage/Time.svg';
 
 export default defineComponent({
   name: 'StructurePage',
+  components: { ContactsBlock, Time },
 
   setup() {
     const mounted = ref(false);
@@ -113,6 +128,49 @@ export default defineComponent({
 }
 :deep(.el-timeline-item__wrapper) {
   width: 100%;
+}
+
+.doctor-name {
+  font-family: Roboto, Verdana, sans-serif;
+  font-size: 18px;
+  color: #343D5C;
+  padding-bottom: 10px;
+  font-weight: bold;
+  margin: 15px 0 0 0;
+}
+p {
+  margin: 0;
+}
+
+.contact-h3 {
+  display: flex;
+  justify-content: left;
+  font-family: Roboto, Verdana, sans-serif;
+  font-size: 12px;
+  font-weight: lighter;
+  color: #4a4a4a;
+  align-content: center;
+  text-align: center;
+  margin-top: 5px;
+}
+
+.item {
+  display: flex;
+  flex-direction: row;
+  padding-right: 10px;
+  width: auto;
+  align-items: center;
+  text-align: left;
+}
+
+.icon-time {
+  margin-left: 1px;
+  width: 20px;
+  height: 20px;
+  fill: #2754eb;
+}
+.time-block {
+  display: block;
 }
 
 </style>
