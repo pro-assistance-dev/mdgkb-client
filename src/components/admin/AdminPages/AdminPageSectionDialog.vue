650698d88d0878a2d94565f8fb7ffe0f05427466
LakkinziMusic <lakkinzimusic@gmail.com>
2023-01-31 17:37:26 +0300

    DocsRestore

--- src/components/admin/AdminPages/AdminPageSectionDialog.vue
+++ src/components/admin/AdminPages/AdminPageSectionDialog.vue
@@ -2,20 +2,21 @@
   <el-dialog v-if="mounted" v-model="isPageSectionDialogActive" top="0" width="90%" destroy-on-close @close="handleClose">
     <el-input v-model="pageSection.name" placeholder="Название раздела страницы"></el-input>
     <WysiwygEditor v-model="pageSection.description" />
-    <!-- <AdminDocumentsForm :document-type="pageSection" /> -->
+    <AdminDocumentsForm :document-type="pageSection" />
   </el-dialog>
 </template>
 
 <script lang="ts">
 import { computed, ComputedRef, defineComponent, onMounted, Ref, ref } from 'vue';
 
+import AdminDocumentsForm from '@/components/AdminDocumentsForm.vue';
 import WysiwygEditor from '@/components/Editor/WysiwygEditor.vue';
 import IPageSection from '@/interfaces/IPageSection';
 import Provider from '@/services/Provider';
 
 export default defineComponent({
   name: 'AdminPageSectionDialog',
-  components: { WysiwygEditor },
+  components: { WysiwygEditor, AdminDocumentsForm },
 
   setup() {
     const pageSection: ComputedRef<IPageSection> = computed(() => Provider.store.getters['pages/pageSection']);
