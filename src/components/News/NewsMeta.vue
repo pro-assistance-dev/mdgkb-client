3e3a0b102a357a7e893ce071e4a3a017cfa503e0
LakkinziMusic <lakkinzimusic@gmail.com>
2023-10-05 21:45:31 +0300

    NewsDateFix

--- src/components/News/NewsMeta.vue
+++ src/components/News/NewsMeta.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="card-meta" style="margin-bottom: 0">
     <div v-if="article" class="date-meta">{{ $dateTimeFormatter.format(news.publishedOn, { month: 'long' }) }}</div>
-    <div v-else class="date-meta">{{ $dateTimeFormatter.format(news.publishedOn, { month: 'long' }) }}</div>
+    <div v-else class="date-meta">
+      {{ $dateTimeFormatter.format(news.publishedOn, { day: '2-digit', month: 'long', year: 'numeric' }) }}
+    </div>
     <div class="icons">
       <div class="icon">
         <EyeOutlined />
