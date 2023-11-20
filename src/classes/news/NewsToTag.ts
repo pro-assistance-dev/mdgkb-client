e94f57152b3f7213e492ff5563f690921d4e6997
LakkinziMusic <lakkinzimusic@gmail.com>
2023-11-21 01:05:00 +0300

    NewsFixes

--- src/classes/news/NewsToTag.ts
+++ src/classes/news/NewsToTag.ts
@@ -1,6 +1,7 @@
 import Tag from '@/classes/news/Tag';
 import INewsToTag from '@/interfaces/news/INewsToTag';
 import ITag from '@/interfaces/news/ITag';
+import ClassHelper from '@/services/ClassHelper';
 export default class NewsToTag {
   id?: string;
   tagId?: string;
@@ -24,6 +25,7 @@ export default class NewsToTag {
 
   static Create(newsId?: string, tagId?: string): NewsToTag {
     const item = new NewsToTag();
+    item.id = ClassHelper.CreateUUID();
     item.newsId = newsId;
     item.tagId = tagId;
     return item;
