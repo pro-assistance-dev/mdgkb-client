ed96f9ac5cb732339cb11299cb05eb591a11c2e1
Daniil <qwer.82@mail.ru>
2023-12-05 20:45:00 +0600

    Upd animation loadmore

--- src/components/LoadMoreButton.vue
+++ src/components/LoadMoreButton.vue
@@ -1,6 +1,11 @@
 <template>
   <div v-if="!allLoaded" class="load-more">
-    <el-button round @click="$emit('loadMore')"> {{ text }}</el-button>
+    <button class="lm-button" @click="$emit('loadMore')">
+      <div v-if="!loading" class="text">{{ text }}</div>
+      <div v-if="loading" class="loadAnimation">
+        <div class="loader" style="--b: 5px; --c: #133dcc; width: 26px; --n: 16"></div>
+      </div>
+    </button>
   </div>
 </template>
 
@@ -31,19 +36,70 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .load-more {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   margin: 20px 0;
   text-align: center;
 }
-:deep(.el-button) {
-  letter-spacing: 1.1px;
-  background-color: inherit;
+
+.lm-button {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  border: 1px solid #133dcc;
+  border-radius: 30px;
+  min-width: 170px;
+  background: #ffffff;
   color: #133dcc;
-  border-color: #133dcc;
+  cursor: pointer;
+  height: 40px;
+  font-size: 15px;
+  letter-spacing: 1.1px;
+  transition: 0.2s;
 }
 
-:deep(.el-button:hover) {
-  background-color: #133dcc;
-  color: white;
-  border-color: white;
+.lm-button:hover {
+  background: #133dcc;
+  color: #ffffff;
+}
+
+.loadAnimation {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+}
+
+.loader {
+  --b: 10px; /* border thickness */
+  --n: 10; /* number of dashes*/
+  --g: 10deg; /* gap  between dashes*/
+  --c: red; /* the color */
+
+  width: 30px; /* size */
+  aspect-ratio: 1;
+  border-radius: 50%;
+  padding: 1px; /* get rid of bad outlines */
+  background: conic-gradient(#0000, var(--c)) content-box;
+  --_m: /* we use +/-1deg between colors to avoid jagged edges */ repeating-conic-gradient(
+      #0000 0deg,
+      #000 1deg calc(360deg / var(--n) - var(--g) - 1deg),
+      #0000 calc(360deg / var(--n) - var(--g)) calc(360deg / var(--n))
+    ),
+    radial-gradient(farthest-side, #0000 calc(98% - var(--b)), #000 calc(100% - var(--b)));
+  -webkit-mask: var(--_m);
+  mask: var(--_m);
+  -webkit-mask-composite: destination-in;
+  mask-composite: intersect;
+  animation: load 1s infinite steps(var(--n));
+}
+@keyframes load {
+  to {
+    transform: rotate(1turn);
+  }
 }
 </style>
