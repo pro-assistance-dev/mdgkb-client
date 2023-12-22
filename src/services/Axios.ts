9f9a6c2621fcfad53ed3cff80962d9db6d48817a
LakkinziMusic <lakkinzimusic@gmail.com>
2023-12-22 13:34:17 +0300

    HandleError

--- src/services/Axios.ts
+++ src/services/Axios.ts
@@ -1,44 +1,12 @@
 import axios from 'axios';
-import { ElNotification } from 'element-plus';
-
-import TokenService from '@/services/Token';
-import store from '@/store';
 
 import HttpError from './classes/HttpError';
 
 const axiosInstance = axios.create();
 
 axiosInstance.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async function (error) {
-    const originalRequest = error.config;
-    if (!error.response) {
-      return;
-    }
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      try {
-        await store.dispatch('auth/refreshToken');
-      } catch (e) {
-        await store.dispatch('auth/logout');
-        return;
-      }
-      axiosInstance.defaults.headers.common['token'] = TokenService.getAccessToken();
-      error.config.headers['token'] = TokenService.getAccessToken();
-      error.config.baseURL = undefined;
-      return axiosInstance.request(originalRequest);
-    }
-    if (error.response.status === 500) {
-      ElNotification({ message: 'Ошибка на сервере, попробуйте позже', duration: 10000, type: 'error' });
-    }
-    if (error.response.status === 400) {
-      const err = new HttpError(error.response.data);
-      ElNotification({ message: err.getErr(), duration: 10000, type: 'error' });
-    }
-    return Promise.reject(error);
-  }
+  (response) => response,
+  async (error) => await HttpError.Handle(error, axiosInstance)
 );
 
 export default axiosInstance;
--- src/services/classes/HttpError.ts
+++ src/services/classes/HttpError.ts
@@ -1,5 +1,10 @@
-import ClassHelper from '../ClassHelper';
+import { AxiosInstance } from 'axios';
+import { ElNotification } from 'element-plus';
 
+import TokenService from '@/services/Token';
+import store from '@/store';
+
+import ClassHelper from '../ClassHelper';
 export default class HttpError {
   private code = 400;
   private err = '';
@@ -14,5 +19,33 @@ export default class HttpError {
 
   private capitalizedErr(): string {
     return this.err.charAt(0).toUpperCase() + this.err.slice(1);
+  }
+
+  static async Handle(error: any, axiosInstance: AxiosInstance) {
+    const originalRequest = error.config;
+    if (!error.response) {
+      return;
+    }
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await store.dispatch('auth/refreshToken');
+      } catch (e) {
+        await store.dispatch('auth/logout');
+        return;
+      }
+      axiosInstance.defaults.headers.common['token'] = TokenService.getAccessToken();
+      error.config.headers['token'] = TokenService.getAccessToken();
+      error.config.baseURL = undefined;
+      return axiosInstance.request(originalRequest);
+    }
+    if (error.response.status >= 500) {
+      ElNotification({ message: 'Ошибка на сервере, попробуйте позже', duration: 10000, type: 'error' });
+    }
+    if (error.response.status >= 400 && error.response.status < 500) {
+      const err = new HttpError(error.response.data);
+      ElNotification({ message: err.getErr(), duration: 10000, type: 'error' });
+    }
+    return Promise.reject(error);
   }
 }
