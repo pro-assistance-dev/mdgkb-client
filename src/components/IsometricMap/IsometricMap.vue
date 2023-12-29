5421cf6cb7d6b02df18d3271747609cbdc11243c
Daniil <qwer.82@mail.ru>
2023-12-29 09:06:48 +0600

    Small fix

--- src/components/IsometricMap/IsometricMap.vue
+++ src/components/IsometricMap/IsometricMap.vue
@@ -83,17 +83,6 @@ function render() {
   renderer.render(scene, camera);
 }
 
-// animate()
-
-// function animate() {
-//   requestAnimationFrame(animate);
-
-//   cube.rotation.x += 0.01;
-//   cube.rotation.y += 0.01;
-
-//   renderer.render(scene, camera);
-// }
-
 onMounted(() => {
   target.value.appendChild(renderer.domElement);
   animate();
