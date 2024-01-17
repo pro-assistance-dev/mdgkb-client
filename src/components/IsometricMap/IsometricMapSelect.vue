e571d290f263d49ca2d3ce174ae46cdd04c82eb5
Daniil <qwer.82@mail.ru>
2024-01-18 01:15:38 +0600

    Upd IsometricMap

--- src/components/IsometricMap/IsometricMapSelect.vue
+++ src/components/IsometricMap/IsometricMapSelect.vue
@@ -1,49 +1,17 @@
 <template>
-  <div v-if="mount">
-    <div class="select-division">
-      <el-select
-        v-model="selectedObjectId"
-        class="select-d"
-        filterable
-        placeholder="Выберите вход, парковку, здание или отделение"
-        style="width: 380px"
-        @change="selectObject"
-      >
-        <template v-for="building in buildings.filter((b) => b.floors.length && b.getFloorsWithDivisions().length > 0)" :key="building">
-          <div class="item-box">
-            <div class="el-select-dropdown__item" style="cursor: default; text-transform: uppercase; color: #a1a7bd">
-              Строение {{ building.number }}
-            </div>
-            <template v-for="floor in building.getFloorsWithDivisions()" :key="floor.id">
-              <div class="el-select-dropdown__item" style="padding-left: 40px; cursor: default; text-transform: uppercase; color: #a1a7bd">
-                Этаж {{ floor.number }}
-              </div>
-              <el-option v-for="division in floor.divisions" :key="division.id" :value="division.id" :label="division.name">
-                <span class="el-select-dropdown__item" style="padding-left: 80px; font-size: 14px; color: #343d5c">{{
-                  division.name
-                }}</span>
-              </el-option>
-            </template>
-          </div>
-        </template>
-        <div class="item-box">
-          <el-option v-for="gate in gates" :key="gate.id" :label="gate.name" :value="gate.id">
-            <span class="el-select-dropdown__item" style="padding-left: 80px; font-size: 14px; color: #343d5c">{{ gate.name }}</span>
+  <div class="select-window">
+    <StringItem string="Выберите точку назначения:" margin="10px" font-size="18px" />
+    <template v-for="building in buildings.filter((b) => b.floors.length && b.getFloorsWithDivisions().length > 0)" :key="building">
+      <div class="item-box">
+        <div style="cursor: default; text-transform: uppercase; color: #a1a7bd">Строение {{ building.number }}</div>
+        <template v-for="floor in building.getFloorsWithDivisions()" :key="floor.id">
+          <div style="padding-left: 40px; cursor: default; text-transform: uppercase; color: #a1a7bd">Этаж {{ floor.number }}</div>
+          <el-option v-for="division in floor.divisions" :key="division.id" :value="division.id" :label="division.name">
+            <span style="padding-left: 80px; font-size: 14px; color: #343d5c">{{ division.name }}</span>
           </el-option>
-        </div>
-        <!--        <div class="item-box">-->
-        <!--          <el-option-group>-->
-        <!--            <el-option v-for="park in parkings" :key="park.value" :label="park.label">-->
-        <!--              <span class="el-select-dropdown__item" style="padding-left: 80px; font-size: 14px; color: #343d5c">{{ park.label }}</span>-->
-        <!--            </el-option>-->
-        <!--          </el-option-group>-->
-        <!--        </div>-->
-      </el-select>
-      <!-- <div class="button-box">
-        <button v-if="showRouterButton" class="route-b" @click="$emit('openMapRouter', selectedObject)">Маршрут</button>
-        <button v-else class="route-b-2" @click="$emit('openMapRouter', selectedObject)">Маршрут</button>
-      </div> -->
-    </div>
+        </template>
+      </div>
+    </template>
   </div>
 </template>
 
@@ -54,12 +22,14 @@ import Building from '@/classes/Building';
 import Division from '@/classes/Division';
 import Floor from '@/classes/Floor';
 import Gate from '@/classes/Gate';
+import StringItem from '@/components/Base/StringItem.vue';
 import IMapObject from '@/interfaces/IMapObject';
 import IOption from '@/interfaces/schema/IOption';
 import FilterQuery from '@/services/classes/filters/FilterQuery';
 import DivisionsFiltersLib from '@/services/Provider/libs/filters/DivisionsFiltersLib';
 import DivisionsSortsLib from '@/services/Provider/libs/sorts/DivisionsSortsLib';
 import Provider from '@/services/Provider/Provider';
+
 const emit = defineEmits(['openMapRouter', 'selectObject']);
 const buildings = computed(() => Provider.store.getters['buildings/items']);
 const gates: Ref<Gate[]> = computed(() => Provider.store.getters['gates/items']);
@@ -96,180 +66,30 @@ onMounted(async () => {
   filterQuery.sortModels.push(DivisionsSortsLib.byName());
   await Provider.store.dispatch('divisions/getAll', { filterQuery });
   await Provider.store.dispatch('entrances/getAll');
+  console.log('массив зданий');
+  console.log(buildings);
   mount.value = true;
 });
 </script>
 
 <style scoped lang="scss">
 @import '@/assets/styles/elements/base-style.scss';
-.map-router-container {
-  display: flex;
-  flex-direction: column;
-  &-item {
-    margin-bottom: 0;
-    display: flex;
-    button {
-      width: 85px;
-      border-radius: 20px;
-      border: none;
-      cursor: pointer;
-      margin-left: 10px;
-      color: white;
-    }
-    .a-btn {
-      background-color: #f3911c;
-      &:hover {
-        background-color: darken(#f3911c, 10%);
-      }
-    }
-    .b-btn {
-      background-color: #006bb5;
-      &:hover {
-        background-color: darken(#006bb5, 10%);
-      }
-    }
-  }
-}
-:deep(.select-d .el-input__inner) {
-  border-radius: 20px;
-  background: #0aa249;
-}
-:deep(.select-d .el-input__inner) {
-  color: #ffffff;
-}
 
-:deep(.select-d .el-input__inner::placeholder) {
-  color: #ffffff;
-}
-
-:deep(.el-select .el-input .el-select__caret) {
-  color: #ffffff;
-  font-size: 15px;
-  font-weight: bold;
-  margin-right: 5px;
-}
-
-:deep(.route-button .el-input__inner) {
-  border-radius: 20px;
+.select-window {
+  position: absolute;
+  z-index: 10;
+  left: 50%;
+  top: 200px;
+  transform: translateX(-50%);
+  min-width: 280px;
+  max-width: 900px;
+  min-height: 300px;
+  max-height: 768px;
+  overflow-y: auto;
   background: #ffffff;
-}
-:deep(.route-button .el-input__inner) {
-  color: $site_dark_gray;
-}
-
-:deep(.route-button .select-d .el-input__inner::placeholder) {
-  color: $site_dark_gray;
-}
-
-:deep(.route-button .el-select .el-input .el-select__caret) {
-  color: $site_dark_gray;
-  font-size: 15px;
-  font-weight: bold;
-  margin-right: 5px;
-}
-
-.route-b {
-  width: 110px;
-  height: 40px;
-  text-decoration: none;
-  display: inline-block;
-  border: none;
-  border-radius: 20px;
-  background-color: #133dcc;
-  color: #ffffff;
-  border-color: #133dcc;
-  cursor: pointer;
-}
-
-.route-b:hover {
-  background-color: lighten(#133dcc, 10%);
-  color: white;
-  border-color: white;
-}
-
-.route-b:active {
-  background-color: lighten(#133dcc, 10%);
-  color: white;
-  border-color: white;
-}
-
-.route-b-2 {
-  width: 110px;
-  height: 40px;
-  text-decoration: none;
-  display: inline-block;
-  border: 1px solid lighten(#133dcc, 20%);
-  border-radius: 20px;
-  background-color: #ffffff;
-  color: lighten(#133dcc, 20%);
-}
-
-.select-division {
-  display: flex;
-  margin: 10px 0;
-}
-
-.description-point {
-  width: 100%;
-  height: 70px;
-  margin-top: 10px;
-  border: 1px solid rgb(black, 0.2);
-  border-radius: $normal-border-radius;
-  background: $base-background;
-}
-
-.route-window {
-  width: 100%;
-  border: 1px solid rgb(black, 0.2);
-  border-radius: $normal-border-radius;
-  background: $base-background;
-  margin-top: 10px;
-  padding-bottom: 15px;
-}
-
-.route-window-title {
-  text-transform: uppercase;
-  margin: 10px 16px;
-  color: $site_gray;
-}
-
-.map-router-container-item {
-  margin-left: 20px;
-}
-
-.choice {
-  display: flex;
-  align-items: center;
-  justify-content: right;
-  height: 20px;
-}
-
-.route-window-line {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.button-field {
-  margin-right: 8px;
-}
-
-.icon-change {
-  width: 15px;
-  height: 15px;
-  fill: #ffffff;
-  stroke: $site_gray;
-  margin-right: 50px;
-}
-
-.icon-change:hover {
-  fill: #ffffff;
-  stroke: $site_dark_gray;
-}
-
-.button-box {
-  width: 110px;
-  margin: 0 0 0 10px;
+  border: 1px solid #838385;
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
 }
 
 .item-box {
