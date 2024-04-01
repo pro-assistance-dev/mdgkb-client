ea035a261015244dbb7e8974b8fc9307647829ac
LakkinziMusic <lakkinzimusic@gmail.com>
2024-04-01 10:25:50 +0300

    CompetitionClean

--- src/components/Educational/AdmissionCommittee/CompetitionComponent.vue
+++ src/components/Educational/AdmissionCommittee/CompetitionComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <div v-if="mounted">
-    <!-- <div class="info"> -->
-    <!--   <span class="info-text" -->
-    <!--     >Для получения информации о конкурсе нажмите -->
-    <!--     <a v-if="isFound" target="_blank" href="/files/contest.pdf" download="Конкурс" class="info-text">сюда</a></span -->
-    <!--   > -->
-    <!--   <!-- <span class="info-text-alt">Информация о конкурсе временно отсутствует</span> -->
-    <!-- </div> -->
+    <div class="info">
+      <!-- <span class="info-text" -->
+      <!--   >Для получения информации о конкурсе нажмите -->
+      <!--   <a v-if="isFound" target="_blank" href="/files/contest.pdf" download="Конкурс" class="info-text">сюда</a></span -->
+      <!-- > -->
+      <!-- <span class="info-text-alt">Информация о конкурсе временно отсутствует</span> -->
+    </div>
     <el-collapse v-model="activeName" accordion @change="collapseChange">
       <!--      <el-collapse-item v-if="UserService.isAdmin()" id="Конкурс" class="card-item" name="Таблица конкурса">-->
       <!--        <template #title>-->
