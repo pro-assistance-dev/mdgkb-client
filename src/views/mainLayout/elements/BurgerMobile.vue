b37602b46d48e41c92e8a0e218e5da2f2fbdb033
Daniil <qwer.82@mail.ru>
2024-05-11 19:29:29 +0500

    Fix path for base-style.scss

--- src/views/mainLayout/elements/BurgerMobile.vue
+++ src/views/mainLayout/elements/BurgerMobile.vue
@@ -131,7 +131,7 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-@import '../../../assets/styles/elements/base-style.scss';
+@import '@/assets/styles/base-style.scss';
 * {
   text-decoration: none;
   color: white;
