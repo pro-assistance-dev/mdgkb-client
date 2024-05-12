0478ef750f8eabfa80570df5c776eb83111d1572
LakkinziMusic <lakkinzimusic@gmail.com>
2024-05-12 10:31:33 +0300

    NewsFooterFix

--- src/components/News/NewsPageFooter.vue
+++ src/components/News/NewsPageFooter.vue
@@ -20,29 +20,17 @@
     </div>
     <div class="bottom-footer">
       <div class="tags-container">
-        <el-tag
-          v-for="newsToTag in news.newsToTags"
-          :key="newsToTag.id"
-          effect="dark"
-          class="news-tag-link"
-          size="small"
-          @click.stop="filterNews(newsToTag.tag)"
-        >
+        <el-tag v-for="newsToTag in news.newsToTags" :key="newsToTag.id" effect="dark" class="news-tag-link"
+          size="small" @click.stop="filterNews(newsToTag.tag)">
           <span>{{ newsToTag.tag.label }}</span>
         </el-tag>
       </div>
       <div class="card-meta share">
         <div style="margin-right: 5px">Поделиться:</div>
-        <ShareNetwork
-          v-for="share in shares"
-          :key="share.name"
-          :network="share.name"
-          :url="getUrl()"
-          :title="news.title"
-          :description="news.title"
-        >
+        <ShareNetwork v-for="share in shares" :key="share.name" :network="share.name" :url="getUrl()"
+          :title="news.title" :description="news.title">
           <div class="share-item">
-            <img class="colored" :src="require(`@/assets/img/social/${share.icon}-colored.webp`)" :alt="share.name" />
+            <img class="colored" :src="'/src/assets/img/social/' + share.icon + '-colored.webp'" :alt="share.name" />
           </div>
         </ShareNetwork>
       </div>
@@ -50,62 +38,52 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { EyeOutlined } from '@ant-design/icons-vue';
-import { defineComponent, PropType } from 'vue';
 
 import News from '@/classes/News';
 import Tag from '@/classes/Tag';
-import NewsLikes from '@/components/News/NewsLike.vue';
 
-export default defineComponent({
-  name: 'NewsPageFooter',
-  components: { EyeOutlined, NewsLikes },
-  props: {
-    news: {
-      type: Object as PropType<News>,
-      required: true,
-    },
+const props = defineProps({
+  news: {
+    type: Object as PropType<News>,
+    required: true,
   },
-  async setup(props) {
-    const shares = [
-      // { name: 'facebook', icon: 'fb' },
-      // { name: 'twitter', icon: 'twitter' },
-      { name: 'VK', icon: 'vk' },
-      { name: 'Telegram', icon: 'telegram' },
-    ];
+})
+const shares = [
+  // { name: 'facebook', icon: 'fb' },
+  // { name: 'twitter', icon: 'twitter' },
+  { name: 'VK', icon: 'vk' },
+  { name: 'Telegram', icon: 'telegram' },
+];
 
-    const filterNews = async (tag: Tag): Promise<void> => {
-      // await Provider.router.push('/news');
-    };
+const filterNews = async (tag: Tag): Promise<void> => {
+  // await Provider.router.push('/news');
+};
 
-    const getUrl = (): string => {
-      const host = process.env.VUE_APP_API_HOST;
-      return `${host}/news/${props.news.id}`;
-    };
-
-    return {
-      shares,
-      getUrl,
-      filterNews,
-    };
-  },
-});
+const getUrl = (): string => {
+  const host = import.meta.env.VITE_APP_API_HOST;
+  return `${host}/news/${props.news.id}`;
+};
 </script>
 
 <style scoped lang="scss">
 .share {
   display: flex;
   align-items: center;
+
   img {
     margin-left: 15px;
     height: 25px;
   }
+
   .anticon {
     margin: 5px;
     font-size: 30px;
   }
+
   .share-item {
+
     /*  .colored {
       display: none;
     } */
@@ -114,36 +92,44 @@ export default defineComponent({
         display: unset;
         transform: scale(1.1);
       }
+
       /* .black {
         display: none;
       } */
     }
   }
 }
+
 .card-meta {
   display: flex;
 }
+
 .article-footer {
   color: #a1a7bd;
 }
+
 .top-footer,
 .bottom-footer {
   display: flex;
   justify-content: space-between;
 }
+
 .top-footer {
   margin-bottom: 15px;
 }
+
 .tags-container {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 }
+
 :deep(.anticon) {
   padding-right: 5px;
   font-size: 20px;
   height: 20px;
 }
+
 .views {
   margin-right: 15px;
   display: flex;
