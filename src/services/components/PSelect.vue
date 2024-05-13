b748a4cdf83562186365285f1feef300f8825b05
Daniil <qwer.82@mail.ru>
2024-05-13 21:22:35 +0500

    clear PSelect

--- src/services/components/PSelect.vue
+++ src/services/components/PSelect.vue
@@ -113,29 +113,13 @@ option {
   outline: 0;
 }
 
-// .text-field__input:disabled,
-// .text-field__input[readonly] {
-//   background-color: $input-readonly-background;
-//   opacity: 1;
-// }
-
-
 .text-field__input::-ms-expand { display: none; } 
  .text-field__input:hover { 
   
   border-color: #888; } 
- .text-field__input:focus { 
-    // border-color: #aaa; 
-    // box-shadow: 0 0 1px 3px rgba(59, 153, 252, .7);
-    // box-shadow: 0 0 0 3px -moz-mac-focusring; 
-    // color: #222;
-    // outline: none; 
-} 
+
  .text-field__input option { 
   
   font-weight:normal; } 
-//  *[dir="rtl"] .text-field__input, :root:lang(ar) .text-field__input, :root:lang(iw) .text-field__input { 
-//     background-position: left .7em top 50%, 0 0; 
-//     padding: .6em .8em .5em 1.4em; 
-// }
+
 </style>
