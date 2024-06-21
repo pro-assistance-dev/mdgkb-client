61a07617b8d8d643417f288c2670deaafad439cd
LakkinziMusic <lakkinzimusic@gmail.com>
2024-06-21 14:29:28 +0300

    UploaderImageDraft

--- src/services/components/UploaderImage.vue
+++ src/services/components/UploaderImage.vue
@@ -69,6 +69,7 @@ const toggleUpload = (file: any) => {
   props.fileInfo.uploadNewFile(file);
   if (props.fileInfo.fileSystemPath) {
     uploadedImg.value = props.fileInfo;
+    uploadedImg.value.url = props.fileInfo.getImageUrl();
     // .push({ name: props.fileInfo.fileSystemPath, url: file.url });
   }
   if (props.withCrop) {
@@ -112,18 +113,32 @@ onBeforeMount(() => {
   console.log(props.fileInfo);
   if (props.fileInfo.fileSystemPath) {
     uploadedImg.value = props.fileInfo;
+    uploadedImg.value.url = props.fileInfo.getImageUrl();
     // fileList.value.push({ name: props.fileInfo.fileSystemPath, url: props.fileInfo.getImageUrl() });
   }
 });
-let fileInput = undefined;
+let fileInput: HTMLInputElement | null = null;
 
 onMounted(() => {
   const dropArea = document.getElementById('drop-area');
-  dropArea.addEventListener('dragover', preventDefaults);
+  if (!dropArea) {
+    return;
+  }
+  dropArea.addEventListener('dragover', (e) => {
+    preventDefaults(e);
+    dropArea.classList.add('drag-over');
+  });
+
+  dropArea.addEventListener('dragleave', (e) => {
+    preventDefaults(e);
+    dropArea.classList.remove('drag-over');
+  });
+
   dropArea.addEventListener('dragenter', preventDefaults);
   dropArea.addEventListener('dragleave', preventDefaults);
   dropArea.addEventListener('drop', handleDrop);
-  fileInput = document.getElementById('file-input');
+
+  fileInput = document.getElementById('file-input') as HTMLInputElement;
 });
 //
 function preventDefaults(e: any) {
@@ -131,27 +146,28 @@ function preventDefaults(e: any) {
   e.stopPropagation();
 }
 const openUploadDialog = () => {
-  fileInput.click();
+  if (fileInput) {
+    fileInput.click();
+  }
 };
-// Handling dropping files into the area
+
 function handleDrop(e: any) {
   e.preventDefault();
   const files = e.dataTransfer.files;
-  console.log(files);
-  if (files.length) {
+  if (files.length && fileInput) {
     fileInput.files = files;
-    // Processing the files for previews (next step)
-    // handleFiles(files);
+    uploadFile();
   }
 }
 const uploadFile = () => {
-  console.log(fileInput.files);
+  if (!fileInput || !fileInput.files || !fileInput.files.length) {
+    return;
+  }
   const file = fileInput.files[0];
   file.url = URL.createObjectURL(file);
   uploadedImg.value = file;
+  props.fileInfo.uploadNewFile(file);
   openCropper(uploadedImg.value);
-  // const f = URL.createObjectURL(file);
-  // console.log(f);
 };
 </script>
 
@@ -164,6 +180,13 @@ const uploadFile = () => {
   line-height: 200px;
   border: 2px dashed #ccc;
   cursor: pointer;
+  &:hover {
+    border: 2px dashed #aaa;
+  }
+}
+
+.drag-over {
+  background-color: #eee;
 }
 
 .inside {
