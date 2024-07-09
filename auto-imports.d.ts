3701393719a177b3a19c0a983dc37bba47641493
LakkinziMusic <lakkinzimusic@gmail.com>
2024-07-09 15:06:01 +0300

    Competition

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -147,7 +147,6 @@ declare module 'vue' {
     readonly Doctor: UnwrapRef<typeof import('@/classes/Main')['Doctor']>
     readonly Dragger: UnwrapRef<typeof import('@/services/Main')['Dragger']>
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
-    readonly ElMessageBox: UnwrapRef<typeof import('element-plus/es')['ElMessageBox']>
     readonly Email: UnwrapRef<typeof import('@/services/Main')['Email']>
     readonly FTSP: UnwrapRef<typeof import('@/services/Main')['FTSP']>
     readonly FaqsStore: UnwrapRef<typeof import('@/store/StoreModules')['FaqsStore']>
@@ -260,7 +259,6 @@ declare module '@vue/runtime-core' {
     readonly Doctor: UnwrapRef<typeof import('@/classes/Main')['Doctor']>
     readonly Dragger: UnwrapRef<typeof import('@/services/Main')['Dragger']>
     readonly EffectScope: UnwrapRef<typeof import('vue')['EffectScope']>
-    readonly ElMessageBox: UnwrapRef<typeof import('element-plus/es')['ElMessageBox']>
     readonly Email: UnwrapRef<typeof import('@/services/Main')['Email']>
     readonly FTSP: UnwrapRef<typeof import('@/services/Main')['FTSP']>
     readonly FaqsStore: UnwrapRef<typeof import('@/store/StoreModules')['FaqsStore']>
--- src/components/Educational/AdmissionCommittee/CompetitionComponent.vue
+++ src/components/Educational/AdmissionCommittee/CompetitionComponent.vue
@@ -1,40 +1,40 @@
 <template>
   <div v-if="mounted">
     <div class="info">
-      <!-- <span class="info-text" -->
-      <!--   >Для получения информации о конкурсе нажмите -->
-      <!--   <a v-if="isFound" target="_blank" href="/files/contest.pdf" download="Конкурс" class="info-text">сюда</a></span -->
-      <!-- > -->
+      <span class="info-text"
+        >Для получения информации о конкурсе нажмите
+        <a v-if="isFound" target="_blank" href="/files/contest.pdf" download="Конкурс" class="info-text">сюда</a></span
+      >
       <span class="info-text-alt">Информация о конкурсе появится позднее</span>
     </div>
-    <!-- <el-collapse v-model="activeName" accordion @change="collapseChange"> -->
-    <!--   <el-collapse-item v-if="UserService.isAdmin()" id="Конкурс" class="card-item" name="Таблица конкурса"> -->
-    <!--     <template #title> -->
-    <!--       <h2>КОНКУРС</h2> -->
-    <!--     </template> -->
-    <!--     <CompetitionTable :residency-courses="residencyCourses" /> -->
-    <!--   </el-collapse-item> -->
-    <!--   <el-collapse-item id="Конкурс на место" class="card-item" name="Конкурс"> -->
-    <!--     <template #title> -->
-    <!--       <h2>КОНКУРС ЧЕЛОВЕК НА МЕСТО</h2> -->
-    <!--     </template> -->
-    <!--     <CompetitionPlacesTable :residency-courses="residencyCourses" /> -->
-    <!--   </el-collapse-item> -->
-    <!---->
-    <!--   <el-collapse-item id="РЕЙТИНГ" class="card-item" name="РЕЙТИНГ"> -->
-    <!--     <template #title> -->
-    <!--       <h2>РЕЙТИНГ АБИТУРИЕНТОВ ПОДАВШИХ ДОКУМЕНТЫ В ОРДИНАТУРУ ГБУЗ «МОРОЗОВСКАЯ ДГКБ ДЗМ»</h2> -->
-    <!--     </template> -->
-    <!--     <CompetitionRating :residency-courses="residencyCourses" /> -->
-    <!--   </el-collapse-item> -->
-    <!---->
-    <!--   <el-collapse-item id="АБИТУРИЕНТЫ" class="card-item" name="АБИТУРИЕНТЫ"> -->
-    <!--     <template #title> -->
-    <!--       <h2>АБИТУРИЕНТЫ, ПОДАВШИЕ ДОКУМЕНТЫ В ОРДИНАТУРУ ГБУЗ «МОРОЗОВСКАЯ ДГКБ ДЗМ»</h2> -->
-    <!--     </template> -->
-    <!--     <CompetitionApplicationsTable :residency-courses="residencyCourses" /> -->
-    <!--   </el-collapse-item> -->
-    <!-- </el-collapse> -->
+    <el-collapse v-model="activeName" accordion @change="collapseChange">
+      <!-- <el-collapse-item v-if="UserService.isAdmin()" id="Конкурс" class="card-item" name="Таблица конкурса"> -->
+      <!--   <template #title> -->
+      <!--     <h2>КОНКУРС</h2> -->
+      <!--   </template> -->
+      <!--   <CompetitionTable :residency-courses="residencyCourses" /> -->
+      <!-- </el-collapse-item> -->
+      <el-collapse-item id="Конкурс на место" class="card-item" name="Конкурс">
+        <template #title>
+          <h2>КОНКУРС ЧЕЛОВЕК НА МЕСТО</h2>
+        </template>
+        <CompetitionPlacesTable :residency-courses="residencyCourses" />
+      </el-collapse-item>
+
+      <el-collapse-item id="РЕЙТИНГ" class="card-item" name="РЕЙТИНГ">
+        <template #title>
+          <h2>РЕЙТИНГ АБИТУРИЕНТОВ ПОДАВШИХ ДОКУМЕНТЫ В ОРДИНАТУРУ ГБУЗ «МОРОЗОВСКАЯ ДГКБ ДЗМ»</h2>
+        </template>
+        <CompetitionRating :residency-courses="residencyCourses" />
+      </el-collapse-item>
+
+      <el-collapse-item id="АБИТУРИЕНТЫ" class="card-item" name="АБИТУРИЕНТЫ">
+        <template #title>
+          <h2>АБИТУРИЕНТЫ, ПОДАВШИЕ ДОКУМЕНТЫ В ОРДИНАТУРУ ГБУЗ «МОРОЗОВСКАЯ ДГКБ ДЗМ»</h2>
+        </template>
+        <CompetitionApplicationsTable :residency-courses="residencyCourses" />
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
